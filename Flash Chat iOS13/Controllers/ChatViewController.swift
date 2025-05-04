7ac75e866943bfcb010a6b59c7aaf6f64e60ec55
23ji <isj4194@naver.com>
2025-05-04 13:01:41 +0900

    메세지 보내기 및 표시 구현

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCore
 
 import UIKit
 
@@ -15,18 +17,28 @@ class ChatViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var messageTextfield: UITextField!
   
-  private var messages: [Message] = [
-    Message(sender: "1@2.com", body: "Hey!"),
-    Message(sender: "1@3.com", body: "Hi!"),
-    Message(sender: "1@5.com", body: "Hello!")
-  ]
+  let db = Firestore.firestore()
+  
+  
+  //MARK: - 원천 데이터
+  
+  private var messages: [Message] = []
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     self.title = "FlashChat"
     self.navigationItem.hidesBackButton = true
+    
+    self.tableView.dataSource = self
+    self.tableView.register(
+      UINib(nibName: K.cellNibName, bundle: nil),
+      forCellReuseIdentifier: K.cellIdentifier
+    )
+    self.loadMessages()
   }
+  
   
   @IBAction func logout(_ sender: UIBarButtonItem) {
     do {
@@ -39,5 +51,58 @@ class ChatViewController: UIViewController {
   
   
   @IBAction func sendPressed(_ sender: UIButton) {
+    guard let messageBody = messageTextfield.text else { return }
+    guard messageBody.isEmpty == false else { return }
+    guard let messageSender = Auth.auth().currentUser?.email else { return }
+    
+    self.db.collection(K.FStore.collectionName).addDocument(data: [
+      K.FStore.senderField: messageSender,
+      K.FStore.bodyField: messageBody
+    ]) { error in
+      guard let error else { return }
+      print(error)
+    }
+    
+    //    self.db.collection(K.FStore.collectionName).addDocument(data: [
+    //      K.FStore.senderField: messageSender,
+    //      K.FStore.bodyField: messageBody
+    //    ])
+  }
+  
+  private func loadMessages () {
+    self.db.collection(K.FStore.collectionName).getDocuments { querySnapShot, error in
+      if let snapShotDocument = querySnapShot?.documents {
+        for doc in snapShotDocument {
+          let data = doc.data()
+          
+          if let sender = data[K.FStore.senderField] as? String,
+             let body = data[K.FStore.bodyField] as? String {
+            let message = Message(sender: sender, body: body)
+            self.messages.append(message)
+          }
+        }
+        DispatchQueue.main.async {
+          self.tableView.reloadData()
+        }
+      }
+    }
+  }
+}
+
+
+extension ChatViewController: UITableViewDataSource {
+  
+  //몇개의 셀을 보여줄지
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.messages.count
+  }
+  
+  //각 셀
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //타입 캐스팅
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as? MessageCell else { return UITableViewCell() }
+    //cell.textLabel?.text = "hi"
+    cell.label.text = self.messages[indexPath.row].body
+    return cell
   }
 }
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by 이상지 on 5/4/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+  
+  @IBOutlet weak var messageBubble: UIView!
+  @IBOutlet weak var label: UILabel!
+  @IBOutlet weak var rightImageView: UIImageView!
+  
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    self.messageBubble.layer.cornerRadius = self.messageBubble.frame.height / 5
+  }
+  
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
